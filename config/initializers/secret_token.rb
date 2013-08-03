f60b51f6d19e5c9f954f4fe53a4c58764c568ddc
Stephen Gonthier <stephen.gonthier@gmail.com>
2013-08-02 17:43:08 -0700

    the begining

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biome::Application.config.secret_key_base = 'cf6020046714686f803ce36710d75c76ef60b565ef7737b7c0a8137830b82dd698d93f4f9292f8a16629da90e57423499d35e6b930472c02e75cce4e54ed303d'
